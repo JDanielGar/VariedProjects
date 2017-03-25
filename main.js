a394d2b76426a9aa8bb196fa5e2665e2d93a3cf5
Daniel Garcia <jdaniel_gar@hotmail.com>
2017-03-24 20:41:17 -0500

    Delete ob file

--- main.js
+++
@@ -1,6 +0,0 @@
-var $myNewElement = $('<p>Nuevo elemento</p>');
-$myNewElement.appendTo('body');
-// Color titulo mensaje clonar y x
-
-var titulo = "Este es un nuevo titulo"
-var interfazChat = $('<div>'+titulo+'</div>')
