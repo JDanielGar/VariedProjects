d8c87f3fb2ee1e6e01a44a9d620538af35f17604
Daniel Garcia <jdaniel_gar@hotmail.com>
2017-03-24 20:38:26 -0500

    Add Chat

---
+++ main.js
@@ -0,0 +1,6 @@
+var $myNewElement = $('<p>Nuevo elemento</p>');
+$myNewElement.appendTo('body');
+// Color titulo mensaje clonar y x
+
+var titulo = "Este es un nuevo titulo"
+var interfazChat = $('<div>'+titulo+'</div>')
